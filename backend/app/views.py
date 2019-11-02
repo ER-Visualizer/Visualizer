ebfcea65b24e781d00fbfcc220ad8882d6ca3611
Simran <snike2610@gmail.com>
2019-11-02 18:01:19 -0400

    updating route

--- backend/app/views.py
+++ backend/app/views.py
@@ -10,10 +10,28 @@ logger = logging.getLogger(__name__)
 def home():
    return "hello world!"
 
+# (self, id=None, process_name=None, distribution=None,
+#  distribution_parameters=None, num_actors=None,
+#                  queue_type=None,priority_function=None,
+#                  output_proceses=None):
+        
+	# "id": 1,
+	# 		"elementType": "triage",
+	# 		"distribution": "gaussian",
+	# 		"distributionParameters": [3,1],
+	# 		"numberOfActors": 10,
+	# 		"queueType": "stack",
+	# 		"priorityFunction": "",
+	# 		"children": [2, 3]
 @app.route('/start', methods=['POST'])
 def start_simulation():
    req_data = request.get_json()
+   elements = req_data['elements']
+   list_of_nodes = []
+   for element in elements:
+      current_node = Node(element[])
    app.logger.info(f"req data {req_data}")
+   
    # call a function from run.py to start simulation
    return send_json_response(req_data)
 
