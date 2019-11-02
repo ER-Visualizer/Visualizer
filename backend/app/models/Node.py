dde74b1766449e5539396a4059e79685ff54f279
nicolae <stroncea.nicolae@gmail.com>
2019-11-02 16:47:22 -0400

    improved node

--- backend/app/models/Node.py
+++ backend/app/models/Node.py
@@ -1,4 +1,32 @@
 class Node():
 
     def __init__(self):
-      pass
+        self.id = None;
+        self.process_name = None
+        self.distribution = None
+        self.distribution_parameters = None
+        self.num_actors = None
+        self.queue_type = None
+        self.priority_function = None
+        self.output_processes  = None
+
+    def set_id(self, id):
+        self.id = id
+
+    def set_process_name(self, process_name):
+        self.process_name = process_name
+
+    def set_distribution(self, distribution, distribution_parameters):
+        self.distribution = distribution
+        self.distribution_parameters = distribution_parameters
+
+    def set_num_actors(self, num_actors):
+        self.num_actors = num_actors
+
+    def set_queue_type(self, queue_type, priority_function=None):
+        self.queue_type = queue_type
+        self.priority_function = priority_function
+
+    def set_output_process(self, output_process):
+        self.output_processes = output_process
+
