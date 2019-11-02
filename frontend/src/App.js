63bd68ebe8801065775e2c944b2622c958d9639c
Victor-Trinh <trinhvictor12@gmail.com>
2019-11-02 16:15:07 -0400

    worked on graph

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as d3 from 'd3';
+import { Graph } from "react-d3-graph";
 
 function App() {
+  let graphical_data = { // graphical representation
+    nodes: [{ id: "Reception0" }, { id: "Triage0" }, { id: "PD0" }],
+    links: [{ source: "Reception0", target: "Triage0" }, { source: "Triage0", target: "PD0" }],
+  };
+
+  let module_data = {
+
+  }
+
+  const myConfig = {
+    nodeHighlightBehavior: true,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    node: {
+        color: "lightgreen",
+        size: 1000,
+        highlightStrokeColor: "blue",
+    },
+    link: {
+        highlightColor: "lightblue",
+    },
+  };
+
+  const onClickNode = function(nodeId){
+    alert(nodeId);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input type="button"/> */}
+      <Graph
+        id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+        data={graphical_data}
+        
+        config={myConfig}
+        onClickNode={onClickNode}
+        directed={true}
+        
+        />
+        
     </div>
   );
 }
+
+// {
+//   data.nodes.push({id: "newnode"})
+//   }
+//   {
+//     data.links.push({source: "newnode", target:"Sally"})
+//   }
 
 export default App;
