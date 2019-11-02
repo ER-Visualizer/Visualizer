e288c22d6450e2505729c212f7f9b49e06ade544
Simran <snike2610@gmail.com>
2019-11-02 17:31:13 -0400

    added code to handle front end input

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,17 +1,33 @@
 # where routes will be
 from app import app
+from flask import request, jsonify
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def home():
    return "hello world!"
 
-@app.route('/',methods=['GET'])
+@app.route('/start', methods=['POST'])
 def start_simulation():
+   req_data = request.get_json()
+   app.logger.info(f"req data {req_data}")
    # call a function from run.py to start simulation
-   pass
+   return send_json_response(req_data)
 
 def function1():
   print("rer")
   print("ded")
   print("dd")
   pass
+
+"""
+Returns a json response.
+"""
+def send_json_response(message: dict):
+    resp = jsonify(message)
+    resp.status_code = 200
+    print(resp)
+    return resp
