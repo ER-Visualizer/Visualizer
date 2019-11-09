5058b87ac43fdd8477da094c09eaba00e885eb64
Daniel Gao <daniel.gao@mail.utoronto.ca>
2019-11-09 13:14:14 -0500

    changed run.py to use synchronous heap implementation

---
+++ backend/app/event.py
@@ -0,0 +1,8 @@
+class Event():
+  def __init__(self, event_time, node_id, node_resource):
+    self.event_time = event_time
+    self.node_id = node_id
+    self.node_resource = node_resource
+      
+  def __lt__(self, other):
+      return self.event_time < other.event_time
---
+++ backend/app/run.py
@@ -0,0 +1,53 @@
+import heapq
+from event import Event
+
+canvas = [{"id": 1}]
+nodes = {}
+event_heap = []
+
+"""
+Setup Canvas:
+
+input = list of nodes [{id: ..., next: [...]}, ... ] as json
+
+def parser(Node[List[nodes]]):
+  set canvas_json as the hard coded demo cases
+"""
+
+def create_queues():
+  for node in canvas:
+    # Each node will create a queue and actors or will I create them here?
+
+    # nodes[node[id]] = Node(node[id], node[process_name], node[distribution], node[distribution_parameters],
+    #                        node[num_actors], node[queue_type], node[priority_function], node[output_processes])
+    pass
+
+"""
+Create event heap
+"""
+def create_heap(heap):
+  heapq.heapify(event_heap)
+
+  # test of push, pop, and replace
+  heapq.heappush(heap, Event(10, 1, 1))
+  heapq.heappush(heap, Event(7, 1, 1))
+  heapq.heappush(heap, Event(4, 1, 1))
+  heapq.heappush(heap, Event(98, 1, 1))
+  heapq.heappush(heap, Event(5, 1, 1))
+  heapq.heappush(heap, Event(0, 1, 1))
+  heapq.heappush(heap, Event(0, 1, 1))
+  heapq.heappush(heap, Event(1, 1, 1))
+  heapq.heappush(heap, Event(0, 1, 1))
+  heapq.heappush(heap, Event(10, 1, 1))
+  heapq.heappush(heap, Event(10, 1, 1))
+  heapq.heappush(heap, Event(10, 1, 1))
+
+  heapq.heappop(heap)
+  heapq.heappop(heap)
+
+  heapq.heapreplace(heap, Event(105, 1, 1))
+
+if __name__ == "__main__":
+  create_heap(event_heap)
+  for e in event_heap:
+    print(e.event_time)
