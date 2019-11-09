b3c24e4096e96128ccaba83a354c80b5588502b7
Mit <mit_rupang@yahoo.ca>
2019-11-09 13:08:34 -0500

    Changes to websocket setup

--- backend/app/connect.py
+++ backend/app/connect.py
@@ -1,34 +1,43 @@
 import asyncio
 import websockets
-import time
+from random import randint, random
+import json
 
-class Websocket:
-    def __init__(self, host, port):
+class WebsocketServer:
+    def __init__(self, host, port, producerFunc):
         self.host = host
         self.port = port
+        self.producerFunc = producerFunc
         self.server = None
 
-    def connect(self):
-        self.server = websockets.serve(ws_handler=handler, 
+    def start(self):
+        self.server = websockets.serve(ws_handler=self.__producer_handler, 
             host=self.host, port=self.port)
+        asyncio.get_event_loop().run_until_complete(self.server)
+        asyncio.get_event_loop().run_forever()
 
     def close(self):
-        pass
+        asyncio.get_event_loop().stop()
 
-# Sample test handler 
-async def hello(websocket, path):
-    name = await websocket.recv()
+    async def __producer_handler(self, websocket, path):
+        while True:
+            message = await self.producerFunc()
+            try:
+                await websocket.send(message)
+            except websockets.exceptions.ConnectionClosed:
+                self.close()
+                break
 
-    greeting = f"Hello {name}!"
-    await websocket.send(greeting)
 
-    while True:
-        await websocket.send(time.ctime())
-        time.sleep(5)
-    
-    print(f"> {greeting}")
 
-# start_server = websockets.serve(ws_handler=hello, host="localhost", port=8765)
 
-# asyncio.get_event_loop().run_until_complete(start_server)
-# asyncio.get_event_loop().run_forever()
+async def producePatientData():
+    await asyncio.sleep(random() * 2)
+    jsonToSend = [{"patientId": 1, "from": randint(1, 10), "to": randint(1, 10)},
+        {"patientId": 2, "from": randint(1, 10), "to": randint(1, 10)},
+        {"patientId": 3, "from": randint(1, 10), "to": randint(1, 10)}]
+    return json.dumps(jsonToSend)
+
+if __name__ == "__main__":
+    server = WebsocketServer("localhost", 8765, producePatientData)
+    server.start()
