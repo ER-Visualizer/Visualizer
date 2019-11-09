d3ab0800eb7b1eec101b0eca67e678173cc04a93
Victor-Trinh <trinhvictor12@gmail.com>
2019-11-09 13:28:25 -0500

    Added a temporary JSON input box

---
+++ frontend/src/components/ManualEntry.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class ManualEntry extends Component {
+
+    state = {
+        layoutJSON: ""
+    }
+
+    sendLayout = () => {
+        let x = new XMLHttpRequest();
+
+        console.log(this.state.layoutJSON);
+
+        if (!this.isValidJSON(this.state.layoutJSON)){
+            alert("invalid JSON");
+            return 
+        }
+
+        // x.open('POST', "URL");
+        // x.send(layoutJSON);
+    };
+
+    isValidJSON = (json) => {
+        try {
+            JSON.parse(this.state.layoutJSON);
+            return true;
+        } catch (e){
+            return false;
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <label>Layout JSON</label>
+                <input type="text" name="JSON_entry" onChange={(e)=> this.setState({layoutJSON : e.target.value.toString()})}></input>
+                <button onClick={this.sendLayout}>Submit</button>
+                
+            </div>
+        )
+    }
+}
