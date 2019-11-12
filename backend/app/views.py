ce11f67a6bea04988a4f0bd73734ea65fdf91f03
Simran <snike2610@gmail.com>
2019-11-11 21:27:20 -0500

    update to views

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,5 +1,10 @@
 # where routes will be
 from app import app
+from flask import request, jsonify
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def home():
@@ -8,20 +13,12 @@ def home():
 @app.route('/start', methods=['POST'])
 def start_simulation():
     req_data = request.get_json()
-    elements = req_data['elements']
-    list_of_nodes = []
-    for element in elements:
-        new_node = Node(element['id'],element['queueType'],
-            element['priorityFunction'], element['numberOfActors'])
-        new_node.set_process_name(element['elementType'])
-        new_node.set_distribution(element['distribution'],element['distributionParameters'])
-        new_node.set_output_process_ids(element['children'])
-        list_of_nodes.append(new_node)
+    
     app.logger.info(f"req data {req_data}")
    
     # TODO: call a function from run.py to start simulation
     return send_json_response(req_data)
-    
+
 """
 Returns a json response.
 """
