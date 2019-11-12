234725d01bd3628309d9addab5c976298c5efdba
nicolae <stroncea.nicolae@gmail.com>
2019-11-12 15:30:40 -0500

    minor improvements to create_csv

--- backend/app/create_csv.py
+++ backend/app/create_csv.py
@@ -7,13 +7,15 @@ with  open("test_file.csv","w") as f:
     writer = csv.writer(f)
     first_row =  ["patient_id","times","patient_acuity"]
     writer.writerow(first_row)
-    delta = datetime.timedelta(minutes=5)
     time_now = datetime.datetime.now()
 
-    while(counter < 20000):
+    while(counter < 1000):
         patient_id = counter
         acuity = randint(1,5)
+        t_for_delta = randint(0,10)
+        delta = datetime.timedelta(minutes=t_for_delta)
+
         time_now += delta
         row = [patient_id, time_now.strftime('%Y-%m-%d %H:%M:%S'), acuity]
         writer.writerow(row)
-        counter+=1
+        counter += 1
