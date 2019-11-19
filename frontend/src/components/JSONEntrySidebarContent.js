b1ff063ba152c95fc7edb6e9231b101fa19d4da6
Victor-Trinh <trinhvictor12@gmail.com>
2019-11-19 17:56:18 -0500

    Styled submit button

--- frontend/src/components/JSONEntrySidebarContent.js
+++ frontend/src/components/JSONEntrySidebarContent.js
@@ -4,15 +4,33 @@ import { connect } from 'react-redux';
 import { replaceNodeList } from '../redux/actions'
 
 export class JSONEntrySidebarContent extends Component {
+    constructor(props){
+        super(props)
+        this.state = {valid: true}
+    }
+
+    handleSubmit(e){
+        
+        try {
+            let validatedJSON = JSON.parse(this.state.layoutJSON)
+            this.props.replaceNodeList(validatedJSON)
+        } catch(e){
+            this.setState({valid: false})
+        }
+
+    }
 
     render() {
         return (
             <div className="JSONEntrySidebarContent">
                 <label>Layout JSON</label><br/>
                 <div>
-                <textarea rows="30" name="JSON_entry" onChange={(e)=> this.setState({layoutJSON : e.target.value.toString()})}></textarea>    
+                <textarea rows="30" name="JSON_entry" onChange={(e) => this.setState({valid: true, layoutJSON : e.target.value.toString()})}></textarea>    
                 </div>
-                <button onClick={() => this.props.replaceNodeList(JSON.parse(this.state.layoutJSON))}>Submit</button>
+                <button className="SubmitJSONButton" onClick={(e) => this.handleSubmit(e)}>Submit</button>
+                <div className="JSONWarningContainer">
+                    <label className="JSONWarningText">{this.state.valid ? "" : "  Invalid JSON"}</label>
+                </div>
                 
             </div>
         )
