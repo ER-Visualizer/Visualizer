b64b8e33896757d8a2bcf6bdf4f62588fb396f45
Mit <mit_rupang@yahoo.ca>
2019-11-24 21:02:46 -0500

    Added colors for different accuities

--- frontend/src/components/Queue.js
+++ frontend/src/components/Queue.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Queue.css';
 import PatientBox from './PatientBox';
+import Patient from '../models/Patient';
 
 const Queue = ({ patients }) => {
-    console.log(patients)
-
+    patients.sort((patient1, patient2) =>
+        patient1.accuity - patient2.accuity);
     const PatientBoxes = patients.map((patient, i) =>
-        <PatientBox key={i} patient={patients[i]} />
+        <PatientBox key={i} patient={patient} />
     );
 
     return (
         <div className="QueueContainer">
-            {PatientBoxes}
+            <div className="Queue">
+                {PatientBoxes}
+            </div>
         </div>
     )
 };
