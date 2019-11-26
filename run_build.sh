5dbd72e42de30bd28eba77266e24713a0c907c00
Carlson Lau <carl.lau999@gmail.com>
2019-11-26 05:53:23 -0500

    fix requirements

--- run_build.sh
+++ run_build.sh
@@ -3,4 +3,4 @@ chmod +x transfer_env_vars.sh
 echo "Transfering .env variables"
 ./transfer_env_vars.sh .env
 echo "Starting docker-compose"
-docker-compose -f docker-compose-prod.yml up --build --no-cache 
+docker-compose -f docker-compose-prod.yml up --build
