cc065d3a06b87883167e9dc46168a5df68824866
Carlson Lau <carl.lau999@gmail.com>
2019-11-26 05:23:47 -0500

    create scripts to deploy production docker and dev docker

---
+++ run_dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod +x transfer_env_vars.sh
+echo "Transfering .env variables"
+./transfer_env_vars.sh .env
+echo "Starting docker-compose"
+docker-compose -f docker-compose.yml up --build
