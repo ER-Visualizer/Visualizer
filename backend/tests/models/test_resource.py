18cd085e8a8afdde0f13a07b4ca456163797e3f4
nicolae <stroncea.nicolae@gmail.com>
2019-12-02 13:11:57 -0500

    clean up tests

--- backend/tests/models/test_resource.py
+++ backend/tests/models/test_resource.py
@@ -1,21 +1,24 @@
-from resource import Resource
-from patient import Patient
+from backend.app.models.resource import Resource
+from backend.app.models.patient import Patient
+from backend.app.models.global_strings import *
+from backend.app.models.event import Event
+
 import unittest
 
 class TestResource(unittest.TestCase):
     
-    def test_get_id(self):
-            res = Resource(3)
-            self.assertEqual(3,res.get_id())
+        def test_get_id(self):
+                res = Resource(3)
+                self.assertEqual(3,res.get_id())
     
-    def test_insert_patient(self):
-            res = Resource(3)
-            p = Patient(3,10)
-            self.assertTrue(p.is_available)
-            res.insert_patient(p,25,10)
-            assert(25 == res.get_finish_time())
-            self.assertFalse(p.is_available)
-        
+        def test_insert_patient(self):
+                res = Resource(3)
+                p = Patient({ID:10,START_TIME:4})
+                self.assertTrue(p.is_available)
+                res.insert_patient(p,1,25,3)        
+                assert(25 == res.get_finish_time())
+                self.assertFalse(p.is_available)
+                                      
 
 
 
